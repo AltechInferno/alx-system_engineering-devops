37e031bd7b172cdc3eeb39efca7cc3f387f5a765
Altech Inferno <altechinferno@gmail.com>
2023-10-26 04:27:18 +0000

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
+""" uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
-    # Get user information
-    user_response = requests.get('{}users/{}'.format(base_url, employee_id))
-    user_data = user_response.json()
-
-    # Get user's TODO list
-    todos_response = requests.get('{}todos?userId={}'.format(base_url, employee_id))
-    todos_data = todos_response.json()
-
-    return user_data, todos_data
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    user_data, todos_data = get_employee_info(employee_id)
-
-    user_name = user_data.get('name')
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task['completed']]
-
-    print("Employee {} is done with tasks({}/{}):".format(user_name, len(completed_tasks), total_tasks))
-
-    for task in completed_tasks:
-        print("\t{}".format(task['title']))
 
 if __name__ == "__main__":
-    main()
 
+    apiUrl = 'https://jsonplaceholder.typicode.com/'
+    input = sys.argv[1]
+    userData = '{}users/{}'.format(apiUrl, input)
+    res = requests.get(userData)
+    obj = res.json()
+    print("Employee {} is done with tasks".format(obj.get('name')), end="")
+
+    todos = '{}todos?userId={}'.format(apiUrl, input)
+    res = requests.get(todos)
+    tasks = res.json()
+    all_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            all_task.append(task)
+
+    print("({}/{}):".format(len(all_task), len(tasks)))
+    for task in all_task:
+        print("\t {}".format(task.get("title")))
