8c788753e065c099617e463e6a6ccc487736d1c3
Altech Inferno <altechinferno@gmail.com>
2023-10-26 04:37:13 +0000

    task2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
+"""JSONPlaceholder API to get information about employee and exports """
+import json
 import requests
 import sys
-import json
 
-def get_employee_info(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
-    # Get user information
-    user_response = requests.get('{}users/{}'.format(base_url, employee_id))
-    user_data = user_response.json()
-
-    # Get user's TODO list
-    todos_response = requests.get('{}todos?userId={}'.format(base_url, employee_id))
-    todos_data = todos_response.json()
-
-    return user_data, todos_data
-
-def export_to_json(employee_id, user_data, todos_data):
-    json_data = {str(employee_id): []}
-    for task in todos_data:
-        json_data[str(employee_id)].append({
-            "task": task['title'],
-            "completed": task['completed'],
-            "username": user_data['username']
-        })
-
-    json_file = '{}.json'.format(employee_id)
-    with open(json_file, 'w') as file:
-        json.dump(json_data, file, indent=4)
-
-    print("Data exported to {}.json".format(employee_id))
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    user_data, todos_data = get_employee_info(employee_id)
-
-    export_to_json(employee_id, user_data, todos_data)
 
 if __name__ == "__main__":
-    main()
+
+    apiUrl = 'https://jsonplaceholder.typicode.com/'
+    userId = sys.argv[1]
+    userData = '{}users/{}'.format(apiUrl, userId)
+    res = requests.get(userData)
+    obj = res.json()
+    username = obj.get('username')
+
+    todos = '{}todos?userId={}'.format(apiUrl, userId)
+    res = requests.get(todos)
+    tasks = res.json()
+    all_task = []
+    for task in tasks:
+        dic_task = {"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": username}
+        all_task.append(dic_task)
+
+    dic0_task = {str(userId): all_task}
+    filename = '{}.json'.format(userId)
+    with open(filename, mode='w') as f:
+        json.dump(dic0_task, f)
