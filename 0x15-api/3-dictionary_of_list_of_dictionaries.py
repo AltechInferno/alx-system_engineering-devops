ae868a11d78cbe4604412b37e51af92cfcc211d6
Altech Inferno <altechinferno@gmail.com>
2023-10-26 04:46:17 +0000

    task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
+"""JSONPlaceholder API to get information about employee and exports"""
 import json
 import requests
 import sys
 
-def get_user_tasks(user_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, user_id)
-    tasks_url = "{}/todos?userId={}".format(base_url, user_id)
-
-    user_response = requests.get(user_url)
-    tasks_response = requests.get(tasks_url)
-
-    if user_response.status_code == 404 or tasks_response.status_code == 404:
-        return None, None
-
-    user_data = user_response.json()
-    tasks_data = tasks_response.json()
-    
-    return user_data, tasks_data
-
-def export_all_tasks_to_json():
-    all_tasks = {}
-
-    for user_id in range(1, 11):
-        user_data, tasks_data = get_user_tasks(user_id)
-        if user_data and tasks_data:
-            user_id_str = str(user_id)
-            username = user_data["username"]
-
-            user_tasks = [{"username": username, "task": task["title"], "completed": task["completed"]} for task in tasks_data]
-
-            all_tasks[user_id_str] = user_tasks
-
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_tasks, json_file, indent=4)
 
 if __name__ == "__main__":
-    export_all_tasks_to_json()
 
+    apiUrl = 'https://jsonplaceholder.typicode.com/'
+    userData = '{}users'.format(apiUrl)
+    res = requests.get(userData)
+    obj = res.json()
+    dic0_task = {}
+    for userData in obj:
+        username = userData.get('username')
+        userId = userData.get('id')
+        todos = '{}todos?userId={}'.format(apiUrl, userId)
+        res = requests.get(todos)
+        tasks = res.json()
+        all_task = []
+        for task in tasks:
+            dic_task = {"username": username,
+                         "task": task.get('title'),
+                         "completed": task.get('completed')}
+            all_task.append(dic_task)
+
+        dic_task[str(userId)] = all_task
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w') as f:
+        json.dump(dic_task, f)
