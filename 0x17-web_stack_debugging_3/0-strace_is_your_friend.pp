2d1fa8e2ad962e4148871848f722a898bdbeeaa0
Altech Inferno <altechinferno@gmail.com>
2023-11-08 23:38:07 +0000

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Fix 500 error 
 
 exec {'replace':
-  path => '/usr/local/bin/:/bin/',
+  path     => '/usr/local/bin/:/bin/',
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
